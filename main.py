463e95cf5df2067217ff7ec5de121742edc64898
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2024-12-03 20:30:33 +0530

    add function to dump results in CSV

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pathlib
 from collections import defaultdict
 from datetime import datetime
-from typing import Annotated, Literal, Optional
+from typing import Annotated, Any, Literal, Optional
 
 import typer
 from pydantic import BaseModel, IPvAnyAddress
@@ -63,6 +63,11 @@ def get_cleaned_log(log: str) -> Log:
     return cleaned_log
 
 
+def add_to_csv(item1: Any, item2: Any) -> None:
+    with open("log_analysis_results.csv", "a+", encoding="utf-8") as f:
+        f.write(f"{item1},{item2}\n")
+
+
 def main(
     file: Annotated[
         pathlib.Path,
@@ -73,12 +78,12 @@ def main(
             file_okay=True,
             dir_okay=False,
         ),
-    ]
+    ],
 ):
+    result_file = "./log_analysis_results.csv"
+    # read the logs
     with open(file, "r", encoding="utf-8") as f:
         logs = f.readlines()
-
-    cleaned_logs = map(get_cleaned_log, logs)
 
     # ip frequency
     ip_freq: defaultdict[str, int] = defaultdict(int)
@@ -87,7 +92,8 @@ def main(
     # failed login attempts
     failed_logins: defaultdict[str, int] = defaultdict(int)
 
-    for log in cleaned_logs:
+    # iterate over the parsed logs
+    for log in map(get_cleaned_log, logs):
         ip_freq[str(log.ip)] += 1
         path_freq[log.path] += 1
         if log.status_code == 401:
@@ -97,6 +103,8 @@ def main(
     print("IP Address           Request Count")
     for ip, freq in sorted(ip_freq.items(), key=lambda x: x[1], reverse=True):
         print(f"{ip:<20} {freq}")
+        # write to file
+        add_to_csv(ip, freq)
     print()
 
     # find the most frequently accessed endpoint
@@ -106,6 +114,8 @@ def main(
             most_common = (path, freq)
     print("Most Frequently Accessed Endpoint:")
     print(f"{most_common[0]} (Accessed {most_common[1]} times)")
+    # write to file
+    add_to_csv(most_common[0], most_common[1])
     print()
 
     # print failed login attempts
@@ -113,6 +123,10 @@ def main(
     print("IP Address           Failed Login Attempts")
     for ip, freq in sorted(failed_logins.items(), key=lambda x: x[1], reverse=True):
         print(f"{ip:<20} {freq}")
+        # write to file
+        add_to_csv(ip, freq)
+    print()
+    print(f"Saved results in {result_file}")
 
 
 if __name__ == "__main__":
